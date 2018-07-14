c19a7c43bdf12c1e94e195c49ffeb8dbf9a5fdab
Lindsay Hill <lindsay.k.hill@gmail.com>
2018-07-13 18:14:34 -0700

    Set autoapprove true

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 # Change Log
 
-# 0.1.0
+## 0.2.0
+
+- Set auto_approve = True for applying changes
+
+## 0.1.0
 
 - First release
--- actions/apply.py
+++ actions/apply.py
@@ -17,7 +17,8 @@ class Apply(action.TerraformBaseAction):
         """
         os.chdir(plan_path)
         self.terraform.terraform_bin_path = terraform_exec
-        return_code, stdout, stderr = self.terraform.apply(plan_path, var_file=variable_files)
+        return_code, stdout, stderr = self.terraform.apply(plan_path, var_file=variable_files,
+                                                           auto_approve=True)
         output = stdout + "\n" + stderr
         if return_code == 0:
             return (True, output)
