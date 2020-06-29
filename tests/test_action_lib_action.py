5ea107f8e65dfbec0f638ec5da213b25338618b8
Adam Kingsley <akingsley@glintinc.com>
2020-06-29 22:20:27 +0200

    Attemp to implement output to tests

--- tests/test_action_lib_action.py
+++ tests/test_action_lib_action.py
@@ -47,7 +47,8 @@ class ActionTestCase(TerraformBaseActionTestCase):
         # Verify the results
         self.assertEqual(result, expected_result)
 
-    def test_check_result_success_with_output(self):
+    @mock.patch("lib.action.Terraform.output")
+    def test_check_result_success_with_output(self, mock_output):
         action = self.get_action_instance({})
 
         # Set terraform variables for test
@@ -72,6 +73,36 @@ class ActionTestCase(TerraformBaseActionTestCase):
 
         # Verify the results
         self.assertEqual(result, expected_result)
+
+    @mock.patch("output.os.chdir")
+    @mock.patch("lib.action.Terraform.output")
+    def test_check_result_success_with_output(self, mock_output, mock_chdir):
+        action = self.get_action_instance({})
+        # Declare test input values
+        test_return_code = 0
+        test_stdout = "Terraform has been successfully initialized!"
+        test_stderr = ""
+        test_plan_path = "/terraform"
+        test_terraform_exec = "/usr/bin/terraform"
+
+        # Declare test Terraform.output return values
+        expected_result = "result"
+        mock_output.return_value = expected_result
+
+        mock_chdir.return_value = "success"
+
+        # Execute the run function
+        result = action.check_result(
+            test_return_code,
+            test_stdout,
+            test_stderr,
+            return_output=True
+        )
+
+        # Verify the results
+        self.assertEqual(result, expected_result)
+        self.assertTrue(mock_output.called)
+        mock_chdir.assert_called_with(test_plan_path)
 
     def test_check_result_fail_with_output(self):
         action = self.get_action_instance({})
